fcf2db894ab02d903f76ac45ce68913e683bacbd
Garuururur <niawan013@gmail.com>
2025-07-30 10:58:06 +0700

    perbaikan supaya gambar muncul

--- gacha.js
+++ gacha.js
@@ -5,36 +5,36 @@ let zonk = { name: "Zonk gak terdaftar 😬", img: "/img/zonk.jpg" };
 
 if (kategori === "pink") {
   waifus = [
-    { name: "Hoshino Ojii-san 🥰", img: "/pink/hoshino.jpg" },
-    { name: "Bocchi Si Autis(tapi bolehlah😋)", img: "/pink/bocchi.jpg" },
-    { name: "Yaemiko Mommy 😳", img: "/pink/yaemiko.jpg" },
-    { name: "Zero Two ak mw 5", img: "/pink/zerotwo.jpg" },
-    { name: "Anya Chaaaann 😫", img: "/pink/anya.jpg" },
-    { name: "Ram uwoogh!!! 🤗", img: "/pink/ram.jpg" },
-    { name: "Chika imut tapi besar 😳", img: "/pink/fujiwara.jpg" },
+    { name: "Hoshino Ojii-san 🥰", img: "pink/hoshino.jpg" },
+    { name: "Bocchi Si Autis(tapi bolehlah😋)", img: "pink/bocchi.jpg" },
+    { name: "Yaemiko Mommy 😳", img: "pink/yaemiko.jpg" },
+    { name: "Zero Two ak mw 5", img: "pink/zerotwo.jpg" },
+    { name: "Anya Chaaaann 😫", img: "pink/anya.jpg" },
+    { name: "Ram uwoogh!!! 🤗", img: "pink/ram.jpg" },
+    { name: "Chika imut tapi besar 😳", img: "pink/fujiwara.jpg" },
 ];
-  zonk = { name: "Wkwkwkw. masnya loh cik 👹👻", img: "/pink/ironi.jpg" };
+  zonk = { name: "Wkwkwkw. masnya loh cik 👹👻", img: "pink/ironi.jpg" };
 } else if (kategori === "hitam") {
     waifus = [
-        { name: "Azusa Nyan~~ 😻", img: "/hitam/azusa.jpg" },
-        { name: "Megumin, Explossionnnn!!! 🎇🤯", img: "/hitam/megumin.jpg" },
-        { name: "Kaguya-sama, Kalau suka ngomong lah 😑", img: "/hitam/kaguya.jpg" },
-        { name: "Kurumi, jauh jauh Karbit 😡", img: "/hitam/kurumi.jpg" },
-        { name: "Waguri, Ini sih Istriku 😘😘", img: "/hitam/waguri.jpg" },
-        { name: "Yuuki, Insect pun Insect lah 🦗🦗 ", img: "/hitam/yuki.jpg" },
+        { name: "Azusa Nyan~~ 😻", img: "hitam/azusa.jpg" },
+        { name: "Megumin, Explossionnnn!!! 🎇🤯", img: "hitam/megumin.jpg" },
+        { name: "Kaguya-sama, Kalau suka ngomong lah 😑", img: "hitam/kaguya.jpg" },
+        { name: "Kurumi, jauh jauh Karbit 😡", img: "hitam/kurumi.jpg" },
+        { name: "Waguri, Ini sih Istriku 😘😘", img: "hitam/waguri.jpg" },
+        { name: "Yuuki, Insect pun Insect lah 🦗🦗 ", img: "hitam/yuki.jpg" },
     ];
-        zonk = { name: "Walah, Gek piye to kang 😹", img: "/hitam/gatot.jpg" };
+        zonk = { name: "Walah, Gek piye to kang 😹", img: "hitam/gatot.jpg" };
 } else if (kategori === "biru") {
     waifus = [
-        { name: "Ai Idol, Semoga panjang umur 🥳", img: "/biru/Ai.jpg" },
-        { name: "Anna, Menang kah? ", img: "/biru/Anna.jpg" },
-        { name: "Aqua, Dasar Dewi Sableng 😿", img: "/biru/aqua.jpg" },
-        { name: "Roxy, Istri Ketiga sih 😘", img: "/biru/Roxy.jpg" },
-        { name: "Ryo, Penghoetank 😩", img: "/biru/ryo.jpg" },
-        { name: "Mikuu, Istrimu Hologram Mass 😩", img: "/biru/Miku.jpg" },
-        { name: "Rem, Kasihan banget, sini sama aku aja😥 ", img: "/biru/Rem.jpg" },
+        { name: "Ai Idol, Semoga panjang umur 🥳", img: "biru/Ai.jpg" },
+        { name: "Anna, Menang kah? ", img: "biru/Anna.jpg" },
+        { name: "Aqua, Dasar Dewi Sableng 😿", img: "biru/aqua.jpg" },
+        { name: "Roxy, Istri Ketiga sih 😘", img: "biru/Roxy.jpg" },
+        { name: "Ryo, Penghoetank 😩", img: "biru/ryo.jpg" },
+        { name: "Mikuu, Istrimu Hologram Mass 😩", img: "biru/Miku.jpg" },
+        { name: "Rem, Kasihan banget, sini sama aku aja😥 ", img: "biru/Rem.jpg" },
     ];
-        zonk = { name: "weladalah rek, Ksatria 300 raja iblis villain 😈😈", img: "/biru/fuad.jpg" };
+        zonk = { name: "weladalah rek, Ksatria 300 raja iblis villain 😈😈", img: "biru/fuad.jpg" };
 } else {
       waifus = [
         { name: "Zonk gak terdaftar 😬", img: "/img/zonk.jpg" }
